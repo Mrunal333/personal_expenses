99b6e0b4c67b5a0fe6d795ef0463e65e81b9ca0a
Mrunal <mrunalsheramkar333@gmail.com>
2020-11-28 23:16:47 +0530

    Manual transaction Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:personal_expenses/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,6 +22,22 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+
+  final List<Transaction> transaction = [
+    Transaction(
+      id: 't1',
+      title: 'new dress',
+      amount: 500.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'weekly Groceries',
+      amount: 2000.00,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
 
@@ -29,25 +46,66 @@ class MyHomePage extends StatelessWidget {
         title: Text('My Expenses'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-        Card(
-          color: Colors.white60,
-          child: Container(
-              width: 100,
-              child: Text('CHART !')),
-              elevation: 10,
+        Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blueAccent,
+                child:Text('CHART !'),
+                 elevation: 10,
         ),
-        Card(
-          color: Colors.white60,
-          child: Container(
-              width: 100,
-              child: Text('LIST OF TX')),
-              elevation: 10,
         ),
+       Column(
+         children: transaction.map((tx) {
+         return Card(
+             child: Row(
+               children: [
+               Container(
+                 margin: EdgeInsets.symmetric(
+                 vertical: 20,
+                 horizontal: 15,
+                 ),
+                 decoration: BoxDecoration(
+                     border: Border.all(
+                         color:  Colors.purple,
+                         width: 2
+                     )
+                 ),
+                 padding: EdgeInsets.all(10),
+                 child: Text('₹ ' + tx.amount.toString(),
+                  /* child: Text('\₹₹{tx.amount}',*/
+                   style: TextStyle(
+                       fontWeight: FontWeight.bold,
+                       fontSize: 16,
+                       color: Colors.purple,
+                   ),
+               ),
+               ),
+               Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children: [
+                   Text(
+                     tx.title,
+                     style: TextStyle(
+                       fontSize: 14,
+                       fontWeight: FontWeight.bold
+                     ),
+                   ),
+                   Text(
+                       tx.date.toIso8601String(),
+                       style: TextStyle(
+                         color: Colors.grey
+                       ),
+                   ),
+               ],
+               ),
+             ],),
+         );
+       }) .toList(),)
         ],
         ),
-
     );
   }
 }
